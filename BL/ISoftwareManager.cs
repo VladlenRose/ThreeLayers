db6f69a2e9cf8f5e8f8b42816d7d91c195d0de83
VladlenRose <115209995+VladlenRose@users.noreply.github.com>
2025-04-11 23:42:14 +0400

    Add files via upload

---
+++ BL/ISoftwareManager.cs
@@ -0,0 +1,27 @@
+﻿using DL.Softwares;
+
+namespace BL
+{
+    public interface ISoftwareManager
+    {
+        SoftwareBase GetSoftware(int id);
+
+        List<SoftwareBase> GetAllSoftwares();
+
+        List<SoftwareBase> GetSoftwaresByType(Type type);
+
+        List<SoftwareBase> FindSoftwaresByName(string name);
+
+        List<SoftwareBase> GetAvailableSoftwares();
+
+        List<SoftwareBase> GetAvailableSoftwaresByType(Type type);
+
+        void AddSoftware(SoftwareBase software);
+
+        void RemoveSoftware(int id);
+
+        void Save(String path);
+
+        void Load(String path);
+    }
+}
---
+++ BL/SoftwareManager.cs
@@ -0,0 +1,85 @@
+﻿using DL.Repository;
+using DL.Softwares;
+
+namespace BL
+{
+    public class SoftwareManager : ISoftwareManager
+    {
+        private ISoftwareRepository repository;
+
+        public SoftwareManager(ISoftwareRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public SoftwareBase GetSoftware(int id)
+        {
+            return repository.GetSoftware(id);
+        }
+
+        public List<SoftwareBase> GetAllSoftwares()
+        {
+            return repository.GetAllSoftwares();
+        }
+
+        public List<SoftwareBase> GetSoftwaresByType(Type type)
+        {
+            return repository.GetSoftwaresByType(type);
+        }
+
+        public List<SoftwareBase> FindSoftwaresByName(string name)
+        {
+            return repository.FindSoftwaresByName(name);
+        }
+
+        public List<SoftwareBase> GetAvailableSoftwares()
+        {
+            var allSoftwares = repository.GetAllSoftwares();
+            var availableSoftwares = new List<SoftwareBase>();
+            foreach (var software in allSoftwares)
+            {
+                if (software.IsAvailable())
+                {
+                    availableSoftwares.Add(software);
+                }
+            }
+
+            return availableSoftwares;
+        }
+
+        public List<SoftwareBase> GetAvailableSoftwaresByType(Type type)
+        {
+            var softwaresByType = repository.GetSoftwaresByType(type);
+            var availableSoftwares = new List<SoftwareBase>();
+            foreach (var software in softwaresByType)
+            {
+                if (software.IsAvailable())
+                {
+                    availableSoftwares.Add(software);
+                }
+            }
+
+            return availableSoftwares;
+        }
+
+        public void AddSoftware(SoftwareBase software)
+        {
+            repository.AddSoftware(software);
+        }
+
+        public void RemoveSoftware(int id)
+        {
+            repository.RemoveSoftware(id);
+        }
+
+        public void Save(string path)
+        {
+            repository.Save(path);
+        }
+
+        public void Load(string path)
+        {
+            repository.Load(path);
+        }
+    }
+}
